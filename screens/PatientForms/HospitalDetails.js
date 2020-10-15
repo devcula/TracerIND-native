d03e6be47715fe3b2a514c3bcd285ce79c8f3a74
Hrituja <hrkhatavkar@gmail.com>
2020-10-15 18:39:18 +0530

    Hospital Details Form

--- screens/PatientForms/HospitalDetails.js
+++ screens/PatientForms/HospitalDetails.js
@@ -1,20 +1,242 @@
-import React from 'react';
+import React, { useState } from 'react'
 import {ScrollView, View, Text, StyleSheet} from 'react-native';
-import {Button} from 'react-native-paper';
+import {Button,TextInput, RadioButton, ThemeProvider} from 'react-native-paper';
+import {Picker} from '@react-native-community/picker';
+import DatePicker from 'react-native-date-picker'
 
 import PatientContext from '../../components/PatientContext';
+
 class HospitalDetails extends React.Component {
   constructor() {
     super();
+    this.state = {date:"2016-05-15"}
   }
+  
 
   render() {
     console.log('Rendering HospitalDetails');
+    
+
     return (
       <ScrollView>
         <View>
-          <Text style={styles.text}>Hospital Details</Text>
+          <Text style={styles.heading}>Hospital Details</Text>
         </View>
+   
+        <View>
+            <Picker
+              selectedValue={this.context.getValue('hospitalAdmit')}
+              onValueChange={(itemValue, itemIndex) => {
+                this.context.saveDataToParent({hospitalAdmit: itemValue});
+              }}>
+              <Picker.Item label="Hospital Admitted in" value="" />
+              <Picker.Item label="PHC/Tulasipaka" value="PHC/Tulasipaka" />
+              <Picker.Item label="PHC/E.D Pally" value="PHC/E.D Pally" />
+              <Picker.Item label="PHC/Laxmipuram" value="PHC/Laxmipuram" />
+              <Picker.Item label="PHC/Gowridevipeta" value="PHC/Gowridevipeta" />
+              <Picker.Item label="PHC/Kuturu" value="PHC/Kuturu" />
+              <Picker.Item label="PHC/Rekhapally" value="PHC/Rekhapally" />
+              <Picker.Item label="PHC/Jeediguppa" value="PHC/Jeediguppa" />
+              <Picker.Item label="AH/Chintoor" value="AH/Chintoor" />
+              <Picker.Item label="AH/Rampachodavaram" value="AH/Rampachodavaram" />
+              <Picker.Item label="AH/Bhadrachalam" value="AH/Bhadrachalam" />
+              <Picker.Item label="DH/Rajamundry" value="DH/Rajamundry" />
+              <Picker.Item label="GGH/Kakinada" value="GGH/Kakinada" />
+            </Picker>
+          </View>
+
+          <View style={styles.rowFlex}>
+          <View style={styles.contentScreen}>
+            <Text style={styles.inputLabel}>Refered to Any Hospital :</Text>
+          </View>
+          <View style={[styles.rowFlex, {flex: 2}]}>
+            <RadioButton.Group
+              onValueChange={(value) =>
+                this.context.saveDataToParent({refered: value})
+              }
+              value={this.context.getValue('refered')}>
+              <View style={styles.contentScreen}>
+                <Text>Yes</Text>
+                <RadioButton color="#14213D" value="yes" />
+              </View>
+              <View style={styles.contentScreen}>
+                <Text>No</Text>
+                <RadioButton color="#14213D" value="no" />
+              </View>
+            </RadioButton.Group>
+          </View>
+          </View>
+
+
+          {(() => {
+            if (this.context.getValue('refered')=== 'yes') {
+              return (
+                <View>
+                <View>
+                <Picker
+                  selectedValue={this.context.getValue('hospitalAdmit')}
+                  onValueChange={(itemValue, itemIndex) => {
+                    this.context.saveDataToParent({hospitalAdmit: itemValue});
+                  }}>
+                  <Picker.Item label="Hospital Refered to" value="" />
+                  <Picker.Item label="AH/Chintoor" value="AH/Chintoor" />
+                  <Picker.Item label="AH/Rampachodavaram" value="AH/Rampachodavaram" />
+                  <Picker.Item label="AH/Bhadrachalam" value="AH/Bhadrachalam" />
+                  <Picker.Item label="DH/Rajamundry" value="DH/Rajamundry" />
+                  <Picker.Item label="GGH/Kakinada" value="GGH/Kakinada" />
+                </Picker>
+              </View>
+              <View>
+                <Text style={styles.inputLabel}>Health Status at the time of referring </Text>
+              </View>
+                  <View>
+              <TextInput
+                style={styles.textinput}
+                value={this.context.getValue('status')}
+                label="Health Status at the time of referring "
+                onChangeText={(value) => {
+                  this.context.saveDataToParent({status: value});
+                }}
+              />
+            </View>
+
+
+            <View style={styles.rowFlex}>
+          <View style={styles.contentScreen}>
+            <Text style={styles.inputLabel}>Need for Dialysis:</Text>
+          </View>
+          <View style={[styles.rowFlex, {flex: 2}]}>
+            <RadioButton.Group
+              onValueChange={(value) =>
+                this.context.saveDataToParent({dialysis: value})
+              }
+              value={this.context.getValue('dialysis')}>
+              <View style={styles.contentScreen}>
+                <Text>Yes</Text>
+                <RadioButton color="#14213D" value="yes" />
+              </View>
+              <View style={styles.contentScreen}>
+                <Text>No</Text>
+                <RadioButton color="#14213D" value="no" />
+              </View>
+            </RadioButton.Group>
+          </View>
+          </View>
+
+
+
+          <View>
+                <Text style={styles.inputLabel}>Treatment Provided </Text>
+              </View>
+                  <View>
+              <TextInput
+                style={styles.textinput}
+                value={this.context.getValue('treatmentDone')}
+                onChangeText={(value) => {
+                  this.context.saveDataToParent({status: value});
+                }}
+              />
+            </View>
+            <View>
+                <Text style={styles.inputLabel}>Recovery Status </Text>
+              </View>
+                  <View>
+              <TextInput
+                style={styles.textinput}
+                value={this.context.getValue('recovery')}
+                onChangeText={(value) => {
+                  this.context.saveDataToParent({recovery: value});
+                }}
+              />
+            </View>
+            </View>
+
+              
+              );
+            }
+            else if(this.context.getValue('refered')=== 'no'){
+              return(
+                <View>
+                  <View>
+              <TextInput
+                style={styles.textinput}
+                value={this.context.getValue('treatmentDone')}
+                label="Treatment Provided"
+                onChangeText={(value) => {
+                  this.context.saveDataToParent({status: value});
+                }}
+              />
+            </View>
+            <View style={styles.rowFlex}>
+          <View style={styles.contentScreen}>
+            <Text style={styles.inputLabel}>Deceased:</Text>
+          </View>
+          <View style={[styles.rowFlex, {flex: 2}]}>
+            <RadioButton.Group
+              onValueChange={(value) =>
+                this.context.saveDataToParent({deceased: value})
+              }
+              value={this.context.getValue('deceased')}>
+              <View style={styles.contentScreen}>
+                <Text>Yes</Text>
+                <RadioButton color="#14213D" value="yes" />
+              </View>
+              <View style={styles.contentScreen}>
+                <Text>No</Text>
+                <RadioButton color="#14213D" value="no" />
+              </View>
+            </RadioButton.Group>
+          </View>
+          </View>
+    
+            
+                </View>
+
+
+              );
+            }
+          })()}
+
+
+    {(() => {
+            if (this.context.getValue('deceased')==='yes') {
+              return (
+                <View>
+                       <View>
+              <TextInput
+                style={styles.textinput}
+                value={this.context.getValue('causeOfDeath')}
+                label="Cause of Death "
+                onChangeText={(value) => {
+                  this.context.saveDataToParent({causeOfDeath: value});
+                }}
+              />
+            </View>
+
+            <View>
+          <TextInput
+            mode="outlined"
+            value={this.context.getValue('placeOfDeath')}
+            label="Place of Death "
+            onChangeText={(value) => {
+              this.context.saveDataToParent({placeOfDeath: value});
+            }}
+            style={styles.textinput}
+          />
+        </View>
+                </View>
+              );
+            }
+          })()}
+          
+
+
+
+
+
+
+
+
         <View style={styles.buttonView}>
           <View style={styles.contentScreen}>
             <Button
@@ -51,12 +273,13 @@ HospitalDetails.contextType = PatientContext;
 const styles = StyleSheet.create({
   contentScreen: {
     flex: 1,
-    // alignItems: 'center',
-    // justifyContent: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  text: {
+  heading:{
     fontWeight: 'bold',
     fontSize: 30,
+
   },
   buttonView: {
     flex: 1,
@@ -64,6 +287,23 @@ const styles = StyleSheet.create({
   },
   buttons: {
     margin: 5,
+  },
+  rowFlex: {
+    flexDirection: 'row',
+  },
+  inputLabel: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 5,
+    marginTop: 5,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  textinput: {
+    marginBottom: 5,
+    marginTop: 5,
+    marginLeft: 10,
+    marginRight: 10,
   },
 });
 
