786f54fb10dae5ce5191791a3675bc607ed4ef34
Abhishek Prasad <devcula@outlook.com>
2020-10-18 00:08:47 +0530

    upload patient

--- screens/LocalPatientList.js
+++ screens/LocalPatientList.js
@@ -16,6 +16,9 @@ import SplashScreen from './SplashScreen';
 import {createStackNavigator} from '@react-navigation/stack';
 import AsyncStorage from '@react-native-community/async-storage';
 
+import axios from 'axios';
+import URI from '../components/URI';
+
 const LocalListStack = createStackNavigator();
 
 function LocalPatientListStackScreen({navigation, navHeaderStyles, userToken}) {
@@ -118,8 +121,24 @@ function LocalPatientList(props) {
         dispatch({type: 'START_SYNC'});
         let dataToSync = state.patientsData;
         for (let i = dataToSync.length - 1; i >= 0; i--) {
-          await new Promise((resolve) => setTimeout(resolve, 800));
-          dataToSync.pop();
+          await new Promise((resolve) => {
+            axios
+              .post(URI + 'AddPatient/', dataToSync[i], {
+                headers: {Authorization: props.userToken.token},
+              })
+              .then((response) => {
+                if (response.status === 200) {
+                  return response.data;
+                }
+              })
+              .then((data) => {
+                //Do some manipulation with data if needed
+                if (dataToSync[i].pkid === data.pkid) {
+                  dataToSync.splice(i, 1);
+                }
+                resolve();
+              });
+          });
         }
         await AsyncStorage.setItem(
           'savedPatientsData',
